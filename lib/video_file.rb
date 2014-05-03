553de28ac043cdff13e5c066d8a67c5dea32fe8f
Zan Loy <zan.loy@gmail.com>
2014-05-03 08:48:48 -0400

    initial init

---
+++ lib/video_file.rb
@@ -0,0 +1,77 @@
+module VideoFile
+    class VideoFile
+        attr_reader :filename, :absolute_path, :basename, :ext, :path, :series_id
+        attr_accessor :title, :description, :director, :imdb_id, :rating, :lang
+        def initialize(path = nil)
+            if path then
+                self.path=(path)
+            end
+        end
+        def path=(path)
+            @absolute_path = File.absolute_path(path)
+            @filename = File.basename(path)
+            @ext = File.extname(path)
+            @basename = File.basename(@filename, @ext)
+        end
+    end
+    class TVShow < VideoFile
+        attr_accessor :episode_name, :first_aired, :tvdb_id, :tvdb_series_id
+        attr_reader :season, :season_str, :episode, :episode_str
+        def initialize(path = nil)
+            @regexes = [
+                /(?<show>.*?)( - |\.)[Ss](?<season>\d{1,2})[Ee](?<episode>\d{1,2}).*/,
+                /(?<show>.*?)[\._ -](?<season>\d)(?<episode>\d{2}).*/,
+                /(?<show>.*?)[\._ -](?<season>\d{1,2})x(?<episode>\d{1,2}).*/,
+            ]
+            super(path)
+        end
+        def path=(path)
+            super(path)
+            @regexes.each { |regex|
+                if result = regex.match(@basename) then
+                    @title = result[:show].gsub(/\./, ' ')
+                    self.season = result[:season]
+                    self.episode = result[:episode]
+                    break
+                end
+            }
+        end
+        def season=(season)
+            if season.class != Fixnum then
+                season = season.to_i
+            end
+            @season_str = "%02d" % season
+            @season = season
+        end
+        def episode=(episode)
+            if episode.class != Fixnum then
+                episode = episode.to_i
+            end
+            @episode_str = "%02d" % episode
+            @episode = episode
+        end
+        def query_tvdb(apikey)
+            require 'thetvdb'
+            client = TheTVDB::Client.new(apikey)
+            client.search(@title)
+            result = client.get_episode_info(@season, @episode)
+            @title = result[:show]
+            @tvdb_id = result[:tvdb_id]
+            @series_id = result[:series_id]
+            @episode_name = result[:name]
+            @first_aired = Date.parse(result[:first_aired])
+            @imdb_id = result[:imdb_id]
+        end
+        def new_filename(pattern: "::title:: - S::season_str::E::episode_str:: - ::episode_name::::ext::", directory: nil)
+            pattern.gsub!( /::(.*?)::/ ) { '#{@'+$1+'}' }
+            pattern.gsub!(/[\/\\?\*:\|"<>]/, '')
+            pattern.gsub!(/  /, ' ')
+            pattern.gsub!(/\.$/, '')
+            if directory then
+                directory.gsub!( /::(.*?)::/ ) { '#{@'+$1+'}' }
+                pattern = File.join(directory, pattern)
+            end
+            eval( '"' + pattern + '"' )
+        end
+    end
+end
---
+++ video_file.gemspec
@@ -0,0 +1,10 @@
+Gem::Specification.new do |s|
+    s.name      = 'video_file'
+    s.version   = '0.1'
+    s.date      = '2013-01-27'
+    s.summary   = 'Generic class to handle video files and query TheTVDB and IMDB for metadata'
+    s.authors   = ['Zan Loy']
+    s.email     = 'zan.loy@gmail.com'
+    s.files     = ["lib/video_file.rb"]
+    s.homepage  = 'http://zanloy.com/scripts/ruby/video_file/'
+end
